56ec269c86b92761e38ccf5cc67008f1c6450dc6
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-02-04 11:18:35 -0800

    add placeholder home content and add emotion for inline styling

---
+++ app-web/src/pages/content.js
@@ -0,0 +1,7 @@
+export const HOMEPAGE = {
+  title: 'CSI Lab',
+  introDescription: `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur vulputate maximus. Sed nibh nisi, sagittis in ex non, elementum suscipit turpis. Cras et nulla eros. Vivamus luctus hendrerit justo vitae semper. Donec nisi nunc, cursus nec tortor id, luctus volutpat turpis. Nunc auctor nisi et felis pellentesque molestie. Sed imperdiet dolor dui, non porta leo euismod a. Morbi eget est malesuada, gravida leo sit amet, sodales eros. Vestibulum pretium elementum enim ut mollis. Nam fermentum mattis lectus, id sodales tortor posuere quis.`,
+};
+
+export const HOMEPAGE_MESSAGES = {};
--- app-web/src/pages/index.js
+++ app-web/src/pages/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 // components
 import Layout from '../hoc/Layout';
+import { Container, Row, Col } from 'reactstrap';
 
 export class Index extends Component {
   render() {
-    return <Layout>hello world</Layout>;
+    return (
+      <Layout>
+        <Container>
+          <Row>
+            <Col md="8" sm="12">
+              Image will be here
+            </Col>
+            <Col md="4" sm={{ size: 6, offset: 3 }}>
+              <main>
+                <h2>CSI Lab</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur vulputate
+                  maximus. Sed nibh nisi, sagittis in ex non, elementum suscipit turpis. Cras et
+                  nulla eros.
+                </p>
+              </main>
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
+    );
   }
 }
 
