3d93c21f8680680eb0905d0e1d8512a67048dce8
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-02-04 10:58:21 -0800

    complete footer

--- app-web/src/utils/createIntlMessage.js
+++ app-web/src/utils/createIntlMessage.js
@@ -1,6 +1,7 @@
 import shortid from 'shortid';
 /**
  * returns a intl message object used by the react-intl librars' formatted message component
+ * more info here https://github.com/yahoo/react-intl/wiki/Components#string-formatting-components
  * @param {String} id 
  * @param {String} message 
  * @param {String} description
